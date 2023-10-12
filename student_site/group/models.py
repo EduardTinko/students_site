e3890759a8a7f704b6da8268b03d962368141441
TinkoEd <228rubiks228@gmail.com>
2023-10-12 23:10:12 +0300

    group mtm

--- student_site/group/models.py
+++ student_site/group/models.py
@@ -17,3 +17,19 @@ class Teacher(models.Model):
 class Group(models.Model):
     name = models.CharField(max_length=50)
     curator = models.ForeignKey(Teacher, on_delete=models.PROTECT)
+
+    def __str__(self):
+        return f"ID: {self.id} Group name: {self.name}"
+
+
+class Student(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    patronymic = models.CharField(max_length=50)
+    birth_date = models.DateField()
+    date_of_admission = models.DateField()
+
+    group = models.ManyToManyField(Group, related_name="students")
+
+    def __str__(self):
+        return f"{self.id} {self.first_name} {self.last_name} {self.patronymic}"
--- student_site/group/views.py
+++ student_site/group/views.py
@@ -1,27 +1,40 @@
 # Create your views here.
 from django.shortcuts import render, redirect
-
-from .forms import GroupForm
+from django.urls import reverse
 
 # Create your views here.
-from .forms import TeacherForm
-from .models import Group
-from .models import Teacher
+from .forms import TeacherForm, GroupForm, StudentForm
+from .models import Group, Teacher, Student
 
 
-def start(reqeust):
-    return render(reqeust, "start.html")
+def start(request):
+    return render(request, "start.html")
 
 
-def teacher_form(reqeust):
-    if reqeust.method == "GET":
+def teacher_form(request):
+    if request.method == "GET":
         form = TeacherForm()
-        return render(reqeust, "teacher_form.html", {"form": form})
-    form = TeacherForm(reqeust.POST)
+        return render(request, "teacher_form.html", {"form": form})
+    form = TeacherForm(request.POST)
     if form.is_valid():
         form.save()
+        return redirect(reverse("teacher_edit", args={form.instance.pk}))
+    return render(request, "teacher_form.html", {"form": form})
+
+
+def teacher_edit(request, pk):
+    teacher = Teacher.objects.get(pk=pk)
+    if request.method == "GET":
+        form = TeacherForm(instance=teacher)
+        return render(request, "teacher_edit.html", {"form": form})
+    form = TeacherForm(request.POST, instance=teacher)
+    if form.is_valid() and "ok" in request.POST:
+        form.save()
         return redirect("teacher_list")
-    return render(reqeust, "teacher_form.html", {"form": form})
+    if "delete" in request.POST:
+        teacher.delete()
+        return redirect("teacher_list")
+    return render(request, "teacher_edit.html", {"form": form})
 
 
 def teacher_list(request):
@@ -29,17 +42,61 @@ def teacher_list(request):
     return render(request, "teacher_list.html", {"teacher": teacher})
 
 
-def group_form(reqeust):
-    if reqeust.method == "GET":
+def group_form(request):
+    if request.method == "GET":
         form = GroupForm()
-        return render(reqeust, "group_form.html", {"form": form})
-    form = GroupForm(reqeust.POST)
+        return render(request, "group_form.html", {"form": form})
+    form = GroupForm(request.POST)
     if form.is_valid():
         form.save()
         return redirect("group_list")
-    return render(reqeust, "group_form.html", {"form": form})
+    return render(request, "group_form.html", {"form": form})
 
 
 def group_list(request):
     group = Group.objects.all()
     return render(request, "group_list.html", {"group": group})
+
+
+def student_form(request):
+    if request.method == "GET":
+        form = StudentForm()
+        return render(request, "student_form.html", {"form": form})
+    form = StudentForm(request.POST)
+    if form.is_valid():
+        form.save()
+        return redirect(reverse("student_edit", args={form.instance.pk}))
+    return render(request, "student_form.html", {"form": form})
+
+
+def student_edit(request, pk):
+    student = Student.objects.get(pk=pk)
+    if request.method == "GET":
+        form = StudentForm(instance=student)
+        return render(request, "student_edit.html", {"form": form})
+    form = StudentForm(request.POST, instance=student)
+    if form.is_valid() and "ok" in request.POST:
+        form.save()
+        return redirect("student_list")
+    if "delete" in request.POST:
+        student.delete()
+        return redirect("student_list")
+    return render(request, "student_edit.html", {"form": form})
+
+
+def student_list(request):
+    student = Student.objects.all()
+    return render(request, "student_list.html", {"student": student})
+
+
+def add_student(request, pk):
+    group = Group.objects.get(pk=pk)
+    if request.method == "GET":
+        form = GroupForm(instance=group)
+        return render(request, "add_student.html", {"form": form, "group": group})
+    form = GroupForm(request.POST, instance=group)
+    if form.is_valid():
+        form.save()
+        form.instance.students.set(form.cleaned_data["students"])
+        return redirect("student_list")
+    return render(request, "add_student.html", {"form": form, "group": group})
